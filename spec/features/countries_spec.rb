7fe45eaa5dd029f451a85a6db2938635f0ffc428
Sean Smith <seansmith1020@gmail.com>
2014-07-21 11:08:34 -0600

    only appropriate countries display when clicking continent

--- spec/features/countries_spec.rb
+++ spec/features/countries_spec.rb
@@ -5,9 +5,14 @@ feature 'Countries and Continents' do
   scenario "User views a list of continents, and sees the countries in that continent" do
     visit '/'
     click_on 'Continents'
+
     click_on 'Africa'
+
     expect(page).to have_content("Kenya")
     expect(page).to have_content("South Africa")
+    expect(page).to_not have_content("United States of America")
+    expect(page).to_not have_content("Italy")
+    expect(page).to_not have_content("Venezuela")
   end
 
 end
